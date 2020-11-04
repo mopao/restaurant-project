c5c6418c2f313c8049c97cac41328b28eeef0fe6
franck cheuzem <franckcheuzem@MacBook-Pro-de-franck.local>
2020-11-04 09:30:28 -0500

    code of loadfooter reviewed

--- js/loadFooter.js
+++ js/loadFooter.js
@@ -2,13 +2,14 @@ $(document).ready(function(){
 
 
   // load the website footer
-  $("footer").load("footer.html",function(responseTxt, statusTxt, xhr){
+  var footer= $("<footer></footer>").load("footer.html",function(responseTxt, statusTxt, xhr){
     if(statusTxt == "success")
       console.log("footer content loaded successfully!");
     if(statusTxt == "error")
       console.log("Error: " + xhr.status + ": " + xhr.statusText);
   });
 
-
+  // add the header to the header to the page's body
+  $("body").append(footer);
 });
 
