14233ef910dbc8f19df86d316315e3f8269c608a
franck cheuzem <franckcheuzem@MacBook-Pro-de-franck.local>
2020-11-04 21:36:07 -0500

    code adding header and footer reviewed

--- js/loadFooter.js
+++ js/loadFooter.js
@@ -1,15 +1,34 @@
 $(document).ready(function(){
 
 
-  // load the website footer
-  var footer= $("<footer></footer>").load("footer.html",function(responseTxt, statusTxt, xhr){
-    if(statusTxt == "success")
-      console.log("footer content loaded successfully!");
-    if(statusTxt == "error")
-      console.log("Error: " + xhr.status + ": " + xhr.statusText);
-  });
+  /***************build the footer***********************************************/
 
-  // add the header to the header to the page's body
+  //declare variables
+  var footer= $("<footer></footer>");
+  var links=[["gift_card","contest"],["contact_us","sitemap"], ["careers","events"]];
+  var arrLinks;
+
+  // build the links section of the footer;
+  var sectionLinks= $("<section></section>");
+  var sectionHeader=$("<h2></h2>").text("important links").addClass("hide");
+  sectionLinks.append(sectionHeader);
+  for(arrLinks of links){
+    var list= $("<ul></ul>");
+    for(var k=0; k<arrLinks.length;k++){
+      var arrTextLink=arrLinks[k].split("_");
+      var link="";
+      for(var j=0; j<arrTextLink.length;j++){
+        link+=arrTextLink[j]+" ";
+      }
+      list.append($("<li></li>").text(link).attr("href",arrLinks[k]+".html"));
+    }
+    sectionLinks.append($("<div></div>").html(list));    
+  }
+  // add the section to the footer
+  footer.append(sectionLinks);
+  // add the copyright to the footer
+  footer.append($("<div></div>").html(" &copy; 2020.").attr("id", "copyright"));
+  // add the footer to the page's body
   $("body").append(footer);
 });
 
--- js/loadHeader.js
+++ js/loadHeader.js
@@ -1,14 +1,78 @@
 $(document).ready(function(){
-
-
-  // load the website header
- var header= $("<header></header>").load("header.html",function(responseTxt, statusTxt, xhr){
-    if(statusTxt == "success")
-      console.log("header content loaded successfully!");
-    if(statusTxt == "error")
-      console.log("Error: " + xhr.status + ": " + xhr.statusText);
-  });
-  // add the header to the header to the page's body
+ 
+ /***************build the header***********************************************/
+ // declare variables
+ 
+ var arrMenulinks=["about_us","menu","specials","locations","place_your_order"];
+ var arrSubMenulinks=["vegetarian_pizza", "meat_pizza", "sides","drinks", "dessert"];
+// create the header
+  var header= $("<header></header>");
+ 
+ // create the banner
+ var banner=$("<div></div>").attr("id","banner-container");
+ // create the sitename 
+ var sitename=$("<h1></h1>").text("pizzarium").attr("id","sitename");
+ // add the sitename into the banner
+ banner.html(sitename);
+ // add the banner to the header
+ header.prepend(banner);
+  // create the menubar
+ var divMenubar=$("<div></div>").attr("id","menu-container");
+ var menubar=$("<nav></nav>").attr("id","main-menu");
+ // create menu header and add it to the menu
+ var menuHeader=$("<h2></h2>").text("Main menu navigation").addClass("hide");
+ menubar.prepend(menuHeader);
+ // create the menu links
+ var listMenu=$("<ul></ul>"); 
+ // create the home logo
+ var homeElt=$("<li></li>");
+ var homeLink=$("<a></a>").attr("href","index.html");
+ homeLink.html($("<span></span>").addClass("fas fa-home").attr("id","home-icon"));
+ homeLink.append($("<span></span>").addClass("hide").text("icon for homepage"));
+ homeElt.html(homeLink);
+ listMenu.html(homeElt);
+ for(var menuLink of arrMenulinks){
+    var elt= $("<li></li>");
+    
+      var arrMenuLink=menuLink.split("_");
+      var link="";
+      for(var j=0; j<arrMenuLink.length;j++){
+        link+=arrMenuLink[j]+" ";
+      }
+      elt.append($("<a></a>").text(link).attr("href",menuLink+".html"));
+  // add submenu of the menu link
+  if(menuLink==="menu"){
+   var sublistMenu=$("<ul></ul>");
+   for(var submenuLink of arrSubMenulinks){
+    var subElt= $("<li></li>");
+    
+      var arrSubMenuLink=submenuLink.split("_");
+      var sublink="";
+      for(var j=0; j<arrSubMenuLink.length;j++){
+        link+=arrSubMenuLink[j]+" ";
+      }
+      subElt.append($("<a></a>").text(link).attr("href",submenuLink+".html"));
+    sublistMenu.append(subElt);
+   }
+   elt.append(sublistMenu);
+  }
+    
+    listMenu.append(elt);    
+  }
+ 
+ // create and add the cart 
+ var cartElt=$("<li></li>").attr("id","shopping-cart");
+ var cart=$("<a></a>").attr("href","#");
+ cart.html($("<i></i>").addClass("fas fa-shopping-cart"));
+ cartElt.html(cart);
+ listMenu.append(cartElt);
+ menubar.html(listMenu);
+ divMenubar.html(menubar);
+ // add the menubar into the header
+ header.append(divMenubar);
+ 
+ 
+  // add the header to the page's body
  $("body").prepend(header);
 
 });
