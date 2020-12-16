6ec681542b581dab468ffb304cd8c5f803e2951b
nastaran <65501452+nastaran-az@users.noreply.github.com>
2020-12-16 11:37:11 -0500

    Add files via upload

--- js/map-location.js
+++ js/map-location.js
@@ -1,19 +1,21 @@
 window.onload=pageReady;
 function pageReady(){
 
-    var direction=document.getElementById("dir");
-   // console.log(direction)
+    var direction=document.getElementById("btnDir");
+    var location=document.getElementById("lmap");
+  
+    
   
     direction.onclick=initMap;
     
     
-    function initMap(){
-      
+     function initMap(){
+           
         var default_view = {
           lat:43.78884669478819 ,
           lng:-79.39145024653189,
         };
-        var location=document.getElementById("map");
+       
         var map = new google.maps.Map(location, {
           zoom: 15,
           center: default_view,
@@ -22,6 +24,7 @@ function pageReady(){
           position: default_view,map,
           title: "PIZZARIUm",
         });
+        
       }
-    } 
+   } 
 
